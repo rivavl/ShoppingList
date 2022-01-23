248ca487caf5a23603a96be5f405581bacb8ebf7
rivavl <mvkalinenkova@yandex.ru>
2022-01-23 21:59:18 +0300

    add fragments dir

---
+++ app/src/main/java/com/marina/shoppinglist/fragments/BaseFragment.kt
@@ -0,0 +1,7 @@
+package com.marina.shoppinglist.fragments
+
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment() {
+    abstract fun onClickNew()
+}
---
+++ app/src/main/java/com/marina/shoppinglist/fragments/FragmentManager.kt
@@ -0,0 +1,15 @@
+package com.marina.shoppinglist.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import com.marina.shoppinglist.R
+
+object FragmentManager {
+    var currentFrag: BaseFragment? = null
+
+    fun setFragment(newFrag: BaseFragment, activity: AppCompatActivity) {
+        val transaction = activity.supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.placeHolder, newFrag)
+        transaction.commit()
+        currentFrag = newFrag
+    }
+}
