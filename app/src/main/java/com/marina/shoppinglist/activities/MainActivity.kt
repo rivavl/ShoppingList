650501b189519e476a71d985fd7f3e335e74cb19
rivavl <mvkalinenkova@yandex.ru>
2022-01-23 21:47:48 +0300

    add bottom nav

--- app/src/main/java/com/marina/shoppinglist/activities/MainActivity.kt
+++ app/src/main/java/com/marina/shoppinglist/activities/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.marina.shoppinglist.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.marina.shoppinglist.R
+import com.marina.shoppinglist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setBottomNavListener()
+    }
+
+    private fun setBottomNavListener() {
+        binding.bNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Log.d("myLog", "Settings")
+                }
+                R.id.notes -> {
+                    Log.d("myLog", "Notes")
+                }
+                R.id.shop_list -> {
+                    Log.d("myLog", "Shop List")
+                }
+                R.id.new_item -> {
+                    Log.d("myLog", "New item")
+                }
+            }
+            true
+        }
     }
 }
